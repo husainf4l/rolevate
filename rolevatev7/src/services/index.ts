7f6a5059e60e161a0e5cd97b668f090a62b15e1c
husain <husain@husains-Mac-Studio.local>
2025-10-29 23:52:46 +0300

    A

--- rolevatev7/src/services/index.ts
+++ rolevatev7/src/services/index.ts
@@ -2,7 +2,6 @@ export { authService } from './auth';
 export { jobsService } from './jobs.service';
 export { applicationService } from './application';
 export { roomService } from './room.service';
-export { reportService } from './report.service';
 
 // Export types
 export * from '@/types/auth';
